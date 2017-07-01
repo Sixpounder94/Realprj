04cbe01b594306bf486b9b7e8206b475eeba28ef
Sixpounder <micheal.fomin@gmail.com>
2017-07-01 06:48:32 -0700

    in progress

--- dairy/views.py
+++ dairy/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    numbers=[1, 2, 3, 4, 5, 6]
+    name = 'SIXPOUNDER'
+
+    args={'name': name, 'numbers': numbers}
+    return render(request, 'home.html', args)
+
